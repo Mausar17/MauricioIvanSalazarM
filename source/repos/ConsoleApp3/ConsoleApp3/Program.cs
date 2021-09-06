43a7ce757bf7723ca108d88ad8370e532fc044a0
Mausar17 <mausar_17@hotmail.com>
2021-09-06 18:01:42 -0500

    First finished version

--- source/repos/ConsoleApp3/ConsoleApp3/Program.cs
+++ source/repos/ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,32 +1,73 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
 using System.IO;
-using OfficeOpenXml;
-namespace ConsoleApp3
+
+namespace FormatoDeCorte
 {
     class Program
     {
         static void Main(string[] args)
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            var file = new FileInfo(@"C:\Users\MauricioIvanSalazarM\Documents\C#Tests\CORTES.xlsx");
 
+            var file = new FileInfo(@"C:\Users\MauricioIvanSalazarM\Downloads\CORTES.xlsx");
+            if (!file.Exists)
+            {
+                Console.WriteLine("Error, no se ha descargado el corte, intentar de nuevo despues de descargar...");
+                return;
+            }
+
+            var listaDeIncidencias = GetIncidenciasList(file);
+
+            AddToTxt(listaDeIncidencias);
+
+            DeleteIfExists(file);
+        }
+
+        /// <summary>
+        /// Adds lista de incidencias to TXT file with formatting, including date
+        /// </summary>
+        /// <param name="incidenciasLista"> Lista de objetos incidencia con propiedades de Folio y ComentarioEjecutivo</param>
+        private static void AddToTxt(List<Incidencias> incidenciasLista)
+        {
+            using (StreamWriter sw = File.AppendText(@"C:\Users\MauricioIvanSalazarM\Documents\Tickets de Cortes.txt"))
+            {
+                sw.WriteLine("\n------------------------------------ " + DateTime.Today.ToString("dddd, dd MMM y"));
+                foreach (var incidencia in incidenciasLista)
+                {
+                    sw.WriteLine(incidencia.FolioTelesoft);
+                    sw.WriteLine(incidencia.ComentarioEjecutivo);
+                    sw.WriteLine("Observación:\n");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets incidencias with folio and comentario ejecutivo from Excel file, saves to list of incidencias objects 
+        /// </summary>
+        /// <param name="file"> Excel File, path gotten from MAIN method</param>
+        /// <returns>List of incidencias objects</returns>
+        private static List<Incidencias> GetIncidenciasList(FileInfo file)
+        {
             using var package = new ExcelPackage(file);
             var worksheet = package.Workbook.Worksheets[0];
-            Console.WriteLine("Worksheet name: " + worksheet.ToString());
-            //Console.WriteLine("Value in cell G4: " + worksheet.Cells["G19"].Value.ToString());
 
-            string textInColumn = "test";
-            int indexOfLastRowWithText = 1;
-            
-            while (true)
+            string comentarioEjecutivo = "";
+            string folioTelesoft = "";
+            int indexOfLastRowWithText = 2; //Starts in row 2, no row 0 in Excel, row 1 has headers
+
+            List<Incidencias> incidenciasLista = new List<Incidencias>(); //list of incidencias objects
+
+            while (true) //Used to populate lista de incidencias "incidenciasLista"
             {
-                var temp = worksheet.Cells["G" + indexOfLastRowWithText.ToString()].Value;
+                var temp = worksheet.Cells["G" + indexOfLastRowWithText.ToString()].Value; //used to check if empty aka NULL
                 if (temp != null)
                 {
-                    textInColumn = temp.ToString();
-                    Console.WriteLine(textInColumn);
+                    comentarioEjecutivo = temp.ToString();
+                    folioTelesoft = worksheet.Cells["A" + indexOfLastRowWithText.ToString()].Value.ToString();
+                    incidenciasLista.Add(new Incidencias { FolioTelesoft = folioTelesoft, ComentarioEjecutivo = comentarioEjecutivo });
                     indexOfLastRowWithText++;
-                    Console.WriteLine(indexOfLastRowWithText);
                 }
                 else
                 {
@@ -34,14 +75,20 @@ namespace ConsoleApp3
                     break;
                 }
             }
-            Console.WriteLine("Last row with text: " + indexOfLastRowWithText);
-            //using (StreamWriter sw = File.AppendText(@"C:\Users\MauricioIvanSalazarM\Documents\Tickets de Cortes 2.txt"))
-            //{
-            //    sw.WriteLine("\n------------------------------------ " + DateTime.Today.ToString("dddd, dd MMM y"));
-            //    sw.WriteLine("This is the new text");
-            //}
 
+            return incidenciasLista;
         }
 
+        /// <summary>
+        /// Deletes Excel when done, otherwise downloading the next corte with the same file name will cause it to become CORTES(1, 2, 3...) which will break program
+        /// </summary>
+        /// <param name="file">FileInfo type, from path to Excel in MAIN</param>
+        private static void DeleteIfExists(FileInfo file)
+        {
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+        }
     }
 }
