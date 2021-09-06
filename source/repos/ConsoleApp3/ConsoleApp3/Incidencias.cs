ffc4bf6e1e5a65ded74d845b66de95caa2e4d48a
Mausar17 <mausar_17@hotmail.com>
2021-09-06 18:04:14 -0500

    First Final version full

---
+++ source/repos/ConsoleApp3/ConsoleApp3/Incidencias.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormatoDeCorte
+{
+    class Incidencias
+    {
+        public string FolioTelesoft { get; set; }
+        public string ComentarioEjecutivo { get; set; }
+    }
+}
